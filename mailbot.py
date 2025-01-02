bb715fd8debcec2edc0ead85be4834fca4343fa0
Aravind Somasundaram <62368290+Aravind96-s@users.noreply.github.com>
2025-01-02 23:27:57 +0530

    Delete mailbot.py

--- mailbot.py
+++
@@ -1,5 +0,0 @@
-import smtplib
-sever =smtplib.SMTP('smtp@gmail.com',587)
-sever.starttls()
-sever.login('aravindrudra1996@gmail.com','Aravind@23#')
-sever.sendmail('aravindrudra1996@gmail.com','awsdemolearn24@gmail.com','demo mail')
